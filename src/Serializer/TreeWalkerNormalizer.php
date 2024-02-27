52109be5710061a4cf30cb896c3efdbb42489199
Ambroise Maupate <ambroise@rezo-zero.com>
2024-02-27 14:15:21 +0100

    refactor: Support Symfony 6/7 for TreeWalkerNormalizer, respect serialization groups

--- src/Serializer/TreeWalkerNormalizer.php
+++ src/Serializer/TreeWalkerNormalizer.php
@@ -6,13 +6,12 @@ namespace RZ\TreeWalker\Serializer;
 
 use RZ\TreeWalker\WalkerInterface;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\SerializerAwareInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-final class TreeWalkerNormalizer implements ContextAwareNormalizerInterface, DenormalizerInterface, SerializerAwareInterface
+final class TreeWalkerNormalizer implements NormalizerInterface, DenormalizerInterface, SerializerAwareInterface
 {
     /**
      * @var DenormalizerInterface&NormalizerInterface
@@ -27,46 +26,60 @@ final class TreeWalkerNormalizer implements ContextAwareNormalizerInterface, Den
         $this->decorated = $decorated;
     }
 
-    public function normalize($object, string $format = null, array $context = [])
+    public function normalize(mixed $object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
         if ($object instanceof WalkerInterface) {
             $type = explode('\\', get_class($object));
-            return [
+            /** @var array<string> $serializationGroups */
+            $serializationGroups = isset($context['groups']) && is_array($context['groups']) ? $context['groups'] : [];
+            $serialized = [
                 '@type' => end($type),
                 'item' => $this->decorated->normalize($object->getItem(), $format, $context),
-                'children' => $object->getChildren()->map(function (WalkerInterface $walker) use ($format, $context) {
+                'children' => $object->getChildren()->map(function (mixed $walker) use ($format, $context) {
                     return $this->normalize($walker, $format, $context);
-                })->toArray(),
-                'level' => $object->getLevel(),
-                'childrenCount' => $object->count(),
-                'index' => $object->getIndex(),
-                'metadata' => $object->getMetadata(),
+                })->getValues(),
             ];
+
+            if (\in_array('walker_level', $serializationGroups, true)) {
+                $serialized['level'] = $object->getLevel();
+            }
+            if (\in_array('children_count', $serializationGroups, true)) {
+                $serialized['childrenCount'] = $object->count();
+            }
+            if (\in_array('walker_metadata', $serializationGroups, true)) {
+                $serialized['metadata'] = $object->getMetadata();
+            }
+            if (\in_array('walker_level', $serializationGroups, true)) {
+                $serialized['level'] = $object->getLevel();
+            }
+            return $serialized;
         }
         return $this->decorated->normalize($object, $format, $context);
     }
 
-    public function supportsNormalization($data, $format = null, array $context = []): bool
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
     {
+        // Symfony 5.4 BC
         return $this->decorated->supportsNormalization($data, $format);
     }
 
-    public function supportsDenormalization($data, $type, $format = null): bool
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
     {
+        // Symfony 5.4 BC
         return $this->decorated->supportsDenormalization($data, $type, $format);
     }
 
     /**
      * @param mixed $data
-     * @param string $class
-     * @param null $format
+     * @param class-string $type
+     * @param string|null $format
      * @param array $context
      * @return mixed
      * @throws ExceptionInterface
      */
-    public function denormalize($data, $class, $format = null, array $context = [])
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): mixed
     {
-        return $this->decorated->denormalize($data, $class, $format, $context);
+        return $this->decorated->denormalize($data, $type, $format, $context);
     }
 
     public function setSerializer(SerializerInterface $serializer): void
@@ -74,5 +87,12 @@ final class TreeWalkerNormalizer implements ContextAwareNormalizerInterface, Den
         if ($this->decorated instanceof SerializerAwareInterface) {
             $this->decorated->setSerializer($serializer);
         }
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            '*' => false,
+        ];
     }
 }
