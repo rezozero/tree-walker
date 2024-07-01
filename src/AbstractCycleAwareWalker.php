a0384e01b909d7a8ba36fccd65ed76d164725895
Ambroise Maupate <ambroise@rezo-zero.com>
2024-07-01 18:33:19 +0200

    fix: Allow `AbstractCycleAwareWalker` to access `getChildren` method more than only once.

--- src/AbstractCycleAwareWalker.php
+++ src/AbstractCycleAwareWalker.php
@@ -11,6 +11,8 @@ use Symfony\Component\Serializer\Annotation as SymfonySerializer;
 
 abstract class AbstractCycleAwareWalker extends AbstractWalker
 {
+    public const MAX_CALL = 3;
+
     #[
         Serializer\Exclude,
         SymfonySerializer\Ignore
@@ -36,7 +38,7 @@ abstract class AbstractCycleAwareWalker extends AbstractWalker
 
     /**
      * @param object|null $item
-     * @return bool Return TRUE if item is not already registered into Walker
+     * @return bool Return TRUE if item has not overpassed the MAX_CALL limit
      */
     protected function registerItem(?object $item): bool
     {
@@ -44,8 +46,12 @@ abstract class AbstractCycleAwareWalker extends AbstractWalker
             return false;
         }
         $itemId = \spl_object_id($item);
-        if (!in_array($itemId, $this->itemIds)) {
-            $this->itemIds[] = $itemId;
+        if (!array_key_exists($itemId, $this->itemIds)) {
+            $this->itemIds[$itemId] = 1;
+            return true;
+        }
+        $this->itemIds[$itemId]++;
+        if ($this->itemIds[$itemId] <= self::MAX_CALL) {
             return true;
         }
         return false;
